fc871879e672a0d0225ce15f861a1751d621120d
TingChung <tingchiun@gmail.com>
2020-11-14 18:24:18 +0800

    Modify build shell

--- platforms/ios/build.sh
+++ platforms/ios/build.sh
@@ -1,1 +1,1 @@
-python  build_framework.py --without calib3d --without features2d --without flann --without gapi --without highgui --without ml --without objdetect --without photo --without stitching --without video --without videoio --without world --without dnn --disable-bitcode --dynamic  output
+python  build_framework.py --without calib3d --without features2d --without flann --without gapi --without highgui --without ml --without objdetect --without photo --without stitching --without video --without videoio --without world --without dnn --disable-bitcode output
