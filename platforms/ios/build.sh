1bb5951c0113f8fc752389a0039a38460f1c693d
TingChung <tingchiun@gmail.com>
2020-11-15 20:49:16 +0800

    Update build shell and build_framework.py

--- platforms/ios/build.sh
+++ platforms/ios/build.sh
@@ -1,1 +1,16 @@
-python  build_framework.py --without calib3d --without features2d --without flann --without gapi --without highgui --without ml --without objdetect --without photo --without stitching --without video --without videoio --without world --without dnn --dynamic output
+python  build_framework.py \
+    --without calib3d \
+    --without features2d \
+    --without flann \
+    --without gapi \
+    --without highgui \
+    --without ml \
+    --without objdetect \
+    --without photo \
+    --without stitching \
+    --without video \
+    --without videoio \
+    --without world \
+    --without dnn \
+    --dynamic \
+    output
